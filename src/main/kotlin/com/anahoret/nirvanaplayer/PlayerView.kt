21512fdf65f01e4278946142bea8c65055a78079
Alexander Mikhalchenko <mikhalchenko.alexander@gmail.com>
2017-09-24 17:13:41 +0300

    Add basic player markup.

--- src/main/kotlin/com/anahoret/nirvanaplayer/PlayerView.kt
+++ src/main/kotlin/com/anahoret/nirvanaplayer/PlayerView.kt
@@ -1,34 +1,14 @@
 package com.anahoret.nirvanaplayer
 
 import com.anahoret.flux.ChangeEvent
-import com.anahoret.flux.Dispatcher
-import com.anahoret.flux.FluxReduceStore
+import com.anahoret.nirvanaplayer.stores.PlayerStore
 import org.jetbrains.react.RProps
 import org.jetbrains.react.RState
 
 import kotlinx.html.*
-import kotlinx.html.js.onClickFunction
 import org.jetbrains.react.ReactComponentSpec
 import org.jetbrains.react.dom.ReactDOMBuilder
 import org.jetbrains.react.dom.ReactDOMComponent
-
-object PlayerDispatcher: Dispatcher()
-
-data class PlayerState(val i: Int)
-object IncAction
-object DecAction
-object PlayerStore: FluxReduceStore<PlayerState>(PlayerDispatcher) {
-
-  override fun getInitialState(): PlayerState = PlayerState(0)
-
-  override fun reduce(state: PlayerState, action: Any): PlayerState {
-    return when(action) {
-      is IncAction -> state.copy(i = state.i + 1)
-      is DecAction -> state.copy(i = state.i - 1)
-      else -> state
-    }
-  }
-}
 
 class PlayerView: ReactDOMComponent<PlayerView.Props, PlayerView.State>() {
   companion object: ReactComponentSpec<PlayerView, Props, State>
@@ -40,7 +20,7 @@ class PlayerView: ReactDOMComponent<PlayerView.Props, PlayerView.State>() {
     PlayerStore.subscribe { event ->
       when (event) {
         is ChangeEvent -> setState {
-          clicks = PlayerStore.getState().i
+          tracks = PlayerStore.getState().tracks
         }
       }
     }
@@ -48,23 +28,26 @@ class PlayerView: ReactDOMComponent<PlayerView.Props, PlayerView.State>() {
   }
 
   override fun ReactDOMBuilder.render() {
-    div {
-      +"It's working!"
-      br{}
-      +"Clicked ${state.clicks} times"
-      br{}
-      button {
-        +"Inc"
-        onClickFunction = { PlayerDispatcher.dispatch(IncAction) }
+    div("player-view") {
+      div("left-panel") {
+        div("player-controls") {
+
+        }
+        div("media-library") {
+
+        }
       }
-      button {
-        +"Dec"
-        onClickFunction = { PlayerDispatcher.dispatch(DecAction) }
+
+      div("right-panel") {
+        div("playlist") {
+          
+        }
       }
+
     }
   }
 
-  class State(var clicks: Int = 0): RState
+  class State(var tracks: List<String> = emptyList()): RState
   class Props: RProps()
 
 }
--- src/main/kotlin/com/anahoret/nirvanaplayer/main.kt
+++ src/main/kotlin/com/anahoret/nirvanaplayer/main.kt
@@ -1,9 +1,12 @@
 package com.anahoret.nirvanaplayer
 
+import com.anahoret.flux.Dispatcher
 import org.jetbrains.react.dom.ReactDOM
 import kotlin.browser.document
 import kotlinx.html.*
 import org.jetbrains.react.dom.render
+
+object PlayerDispatcher: Dispatcher()
 
 fun main(args: Array<String>) {
   ReactDOM.render(document.getElementById("player-root")) {
